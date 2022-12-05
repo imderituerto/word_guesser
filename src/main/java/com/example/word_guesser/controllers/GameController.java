0ac361f30387e27fac3fb938d50d6b4b746c232d
Isabel Martinez de Rituerto <imderituerto@Isabels-MacBook-Pro.local>
2022-12-05 13:41:06 +0100

    second commit

--- src/main/java/com/example/word_guesser/controllers/GameController.java
+++ src/main/java/com/example/word_guesser/controllers/GameController.java
@@ -9,16 +9,32 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 @RestController
 @RequestMapping(value = "/games")
 public class GameController {
 
+    private Game game;
+    private String currentWord;
+    private ArrayList<String> guessedLetters;
+
+    //post request
     @PostMapping
     public ResponseEntity<Reply> newGame(){
-        Game game = new Game("hello", 0, false);
+        this.game = new Game("hello", 0, false);
+        this.currentWord = "*****";
+        this.guessedLetters = new ArrayList<>();
         Reply reply = new Reply("*****", "New game started");
         return new ResponseEntity<>(reply, HttpStatus.CREATED);
     }
 
+    //get request
+    @GetMapping
+    public ResponseEntity<Reply> getGameStatus(){
+        Reply reply = new Reply(this.currentWord, "Game in progress");
+        return new ResponseEntity<>(reply, HttpStatus.OK);
+    }
 
 }
